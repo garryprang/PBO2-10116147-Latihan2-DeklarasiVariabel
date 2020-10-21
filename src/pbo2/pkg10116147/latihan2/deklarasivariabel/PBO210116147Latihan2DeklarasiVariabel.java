331c37f1126ba7862c12276fc9dd80f679e458ac
grryv <grryv@grry>
2020-10-21 19:22:00 +0700

    Tugas - Latihan Pertemuan 1 Garry Prang 10116147 PBO-2

---
+++ src/pbo2/pkg10116147/latihan2/deklarasivariabel/PBO210116147Latihan2DeklarasiVariabel.java
@@ -0,0 +1,36 @@
+package pbo2.pkg10116147.latihan2.deklarasivariabel;
+
+/**
+ * 
+ * @author
+ * Nama                 : Garry Prang
+ * NIM                  : 10116147
+ * Kelas                : PBO-2
+ * Deskripsi Program    : Program untuk pendeklarasian dan pengaksesan variabel.
+ *
+ */
+
+public class PBO210116147Latihan2DeklarasiVariabel {
+
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai    = " + nilaiInt);
+        System.out.println("Isi variabel PHI      = " + PHI);
+        System.out.println("Isi variabel logika   = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+        
+    }
+    
+}
